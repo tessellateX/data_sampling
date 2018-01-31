4fbc7ef4cd72aeffb103ba18de0e2266848ec1ce
aishik-pyne <aishikpyne@gmail.com>
2018-01-31 15:09:13 +0530

    split function modified, mouza re-splitted, mouzas file imported directly from tables

--- splitting data/split.py
+++ splitting data/split.py
@@ -1,25 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jan 29 19:51:14 2018
-
-@author: ANURAN
-"""
-
 import pandas as pd
 
-df=pd.read_csv('mouzas.csv')
-
+df = pd.read_csv('../tables/csv/mouzas.csv')
+df = df[['id', 'name', 'jl_number', 'block_id']]
 for i in range(0,21):
-    name="split_data/split_mouzas"+str(i)+".csv" # path to split file
-    print("Split "+str(i)+" started...")
-    df.iloc[2000*i:2000*(i+1)].to_csv(name,sep=',',encoding='utf-8')
-    print("Split "+str(i)+" completed")
-
-i=i+1
-name="split_data/split_mouzas"+str(i)+".csv" # path to split file
-print("Split "+str(i)+" started...")
-df.iloc[2000*i:].to_csv(name,sep=',',encoding='utf-8')
-print("Split "+str(i)+" completed")
+    path="split_data/split_mouzas"+str(i)+".csv" # path to split file
+    df.iloc[2000*i:2000*(i+1)].to_csv(path,sep=',',encoding='utf-8', index=False)
+    print("\rSplit {} completed".format(i))
 
 print("Splitting done")
-
